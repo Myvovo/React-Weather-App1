b483c2788bd767804de3b48bc0f31167346faafd
Myvovo <138823037+Myvovo@users.noreply.github.com>
2023-10-11 01:10:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Weather from "./Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Weather city="" />
+      This project is Open Sourced by Eva Mbhele
+      <link />
+    </div>
+  );
+}
